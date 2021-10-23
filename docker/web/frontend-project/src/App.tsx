b32dd3079f9ec660fa80dc274ff0262b1d3698cc
unknown <sdelacruz@scu.edu>
2021-10-23 14:44:11 -0700

    current react stuff

--- docker/web/frontend-project/src/App.tsx
+++ docker/web/frontend-project/src/App.tsx
@@ -1,18 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Menu from "./components/MainMenu";
 
 function App() {
+    const options = ["2","3"];
     return (
         <div className="App">
-            <header className="App-header">
-            <NavBar />
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-            </header>
+        <header> 
+            <img src={logo} className="App-logo" alt="logo" />
+            <Menu name="1" options={options} />
+        </header>
+     
+        <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+        </p>
         </div>
     );
 }
---
+++ docker/web/frontend-project/src/components/MainMenu.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+export default function BasicMenu(props:any) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls="basic-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        {props.name}
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}>
+          
+        {props.options.map(option=><MenuItem onClick={handleClose}>{option}</MenuItem>}
+      </Menu>
+    </div>
+  );
+}
